cbc9db5c3c67a26d1698bbacf3739b4e738e2f69
João R. M. Nicola <joaoraf@gmail.com>
2013-09-10 14:21:41 -0300

    Removidas várias dependências desnecessárias no pom.xml

--- symbolicobject/src/main/scala/br/gov/lexml/symbolicobject/util/CollectionUtils.scala
+++ symbolicobject/src/main/scala/br/gov/lexml/symbolicobject/util/CollectionUtils.scala
@@ -3,56 +3,69 @@ package br.gov.lexml.symbolicobject.util
 import scala.collection.IterableLike
 import scala.collection.SeqLike
 import scala.collection.generic.CanBuildFrom
+import scala.collection.generic.IsTraversableOnce
+import scala.collection.GenTraversableLike
 
 object CollectionUtils {
-    final implicit class M2Merge[K,V](m1 : Map[K,V]) {
-      def mergeWith(f : (V,V) => V) = (m2 : Map[K,V]) => mergeMapWith(f)(m1,m2)		   
+  final implicit class M2Merge[K, V](m1: Map[K, V]) {
+    def mergeWith(f: (V, V) => V) = (m2: Map[K, V]) => mergeMapWith(f)(m1, m2)
+  }
+  def mergeMapWith[K, V](f: (V, V) => V) = (m1: Map[K, V], m2: Map[K, V]) =>
+    (m1.toSeq ++ m2.toSeq).groupBy(_._1).mapValues {
+      case Seq(kv) => kv._2
+      case Seq(kv1, kv2) => f(kv1._2, kv2._2)
     }
-	def mergeMapWith[K,V](f : (V,V) => V) = (m1 : Map[K,V], m2 : Map[K,V]) =>
-		(m1.toSeq ++ m2.toSeq).groupBy(_._1).mapValues {
-		  case Seq(kv) => kv._2
-		  case Seq(kv1,kv2) => f(kv1._2,kv2._2)
-		}   
-	def mergeMapsWith[K,V](f : (V,V) => V) = (ml : Iterable[Map[K,V]]) => 	  
-      ml.foldLeft(Map[K,V]())(_.mergeWith(f)(_))
-	
-	final implicit class I2G[K,V](l : Iterable[(K,V)]) {
-	  def groupBy1on2with[C](f : Iterable[V] => C) = l.groupBy(_._1).mapValues(l => f(l.map(_._2)))
-	}
-	/*def groupBy1on2with[K,V,C](f : Iterable[V] => C) = (l : Iterable[(K,V)]) =>
+  def mergeMapsWith[K, V](f: (V, V) => V) = (ml: Iterable[Map[K, V]]) =>
+    ml.foldLeft(Map[K, V]())(_.mergeWith(f)(_))
+
+  final implicit class I2G[K, V](l: Iterable[(K, V)]) {
+    def groupBy1on2with[C](f: Iterable[V] => C) = l.groupBy(_._1).mapValues(l => f(l.map(_._2)))
+  }
+  /*def groupBy1on2with[K,V,C](f : Iterable[V] => C) = (l : Iterable[(K,V)]) =>
 	  l.groupBy(_._1).mapValues(l => f(l.map(_._2))) */
-	
-    def zipAll[A](l : Seq[Seq[A]]) : IndexedSeq[IndexedSeq[Option[A]]] = {
-      val ll = l.map(_.toIndexedSeq).toIndexedSeq
-      val numRows = ll.map(_.length).max
-      (0 until numRows) map { row =>
-        ll.map(d => d.lift(row))
-      }      
+
+  def zipAll[A](l: Seq[Seq[A]]): IndexedSeq[IndexedSeq[Option[A]]] = {
+    val ll = l.map(_.toIndexedSeq).toIndexedSeq
+    val numRows = ll.map(_.length).max
+    (0 until numRows) map { row =>
+      ll.map(d => d.lift(row))
     }
-    
-    def intersperse[T, C <: IterableLike[T,C]](l : C, middle : C)(implicit cb : CanBuildFrom[C,T,C]) = {
-      val b = cb()
-      var l1 = l
-      while(!l1.isEmpty) {
-        val h = l1.head
-        b += h
-        val t = l1.tail
-        if(!t.isEmpty) {
-          b ++= middle
-        }
-    	l1 = t
+  }
+
+  def intersperse[T, C <: IterableLike[T, C]](l: C, middle: C)(implicit cb: CanBuildFrom[C, T, C]) = {
+    val b = cb()
+    var l1 = l
+    while (!l1.isEmpty) {
+      val h = l1.head
+      b += h
+      val t = l1.tail
+      if (!t.isEmpty) {
+        b ++= middle
       }
-      
-      b.result()
+      l1 = t
     }
-    
-    def splitBy[T](l : Seq[T], f : T => Boolean) : IndexedSeq[IndexedSeq[T]] = {
-            
-      val (ll,rest) = l.foldLeft(IndexedSeq[IndexedSeq[T]](),IndexedSeq[T]()) { 
-        case ((ll,yy),x) if f(x) => (ll :+ yy, IndexedSeq[T]())           
-        case ((ll,yy),x) => (ll, yy :+ x)                 
-      }
-      if(rest.isEmpty) { ll }
-      else (ll :+ rest)
+
+    b.result()
+  }
+
+  def splitBy[T](l: Seq[T], f: T => Boolean): IndexedSeq[IndexedSeq[T]] = {
+
+    val (ll, rest) = l.foldLeft(IndexedSeq[IndexedSeq[T]](), IndexedSeq[T]()) {
+      case ((ll, yy), x) if f(x) => (ll :+ yy, IndexedSeq[T]())
+      case ((ll, yy), x) => (ll, yy :+ x)
     }
+    if (rest.isEmpty) { ll }
+    else (ll :+ rest)
+  }
+
+  implicit class SelectableCollection[A, Repr](val r: GenTraversableLike[A, Repr]) {
+    final def select[T <: A](implicit m: Manifest[T],
+      fr: IsTraversableOnce[Repr],
+      cbf: CanBuildFrom[Repr, T, Repr { type A = T }]): Repr { type A = T } =
+      r collect { case x: T => x }
+  }
+
+  implicit class IntersectingMap[K, V1](val m: Map[K, V1]) {
+    final def intersect[V2](m1: Map[K, V2]): Map[K, (V1, V2)] = m.seq.flatMap { case (k, v1) => m1.get(k).map(v2 => (k, (v1, v2))) }
+  }
 }
