bd7ab4a19a0afa79043e22797a62225f57393245
laurocesar <laurocesar@gmail.com>
2014-02-27 18:26:18 -0300

    Imprime a URL gerada internamente

--- qc-presentation/src/main/java/br/gov/camara/quadrocomparativo/main/AppNoFX.java
+++ qc-presentation/src/main/java/br/gov/camara/quadrocomparativo/main/AppNoFX.java
@@ -139,6 +139,8 @@ public class AppNoFX  {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    	System.out.println(URL);
+
     	startServer();
     }
 }
